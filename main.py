e3acaccd00a8a44aa974bf9ea72b85a54c0838cd
Albina Hominyatich <>
2022-10-29 13:54:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import*
+shape('turtle') #arrow, circle, square
+speed(1)
+pensize(6)
+color('red', 'blue')
+
+fd(100)#forward
+left(90)
+fd(75)
+left(90)
+fd(100)#forward
+left(90)
+fd(75)
+left(90)
+
